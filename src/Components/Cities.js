b5f7ee9a5829b0d5a0d5ff3ed2ad3692af49a496
Sule Tekin <suleetekin99@gmail.com>
2023-07-06 20:42:23 -0500

    css updates

--- src/Components/Cities.js
+++ src/Components/Cities.js
@@ -1,37 +1,44 @@
-import Col from "react-bootstrap/Col";
-import NavBar from "./NavBar";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Card from 'react-bootstrap/Card';
-import '../App.css';
-import data from './Data/cities.json';
-
+import React from "react";
+import { Col, Container, Row, Card } from "react-bootstrap";
+import data from "./Data/cities.json";
 
 function Cities() {
   return (
-    <div className= "backgroundCities">
-        <br></br>
-        <div style={{ border: '10px solid white', padding: '1px', backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-        <p style= {{fontSize: 50, textAlign: "center", fontWeight: "800", margin: "auto"}}>Discover Top-Rated Cities Around the World!</p>
-        </div>
-        <br></br>
-        <div>
-            {
-                data.map((data, i) => (
-                    <Card style={{width: "80%", margin: "100px"}} border={'success '} bg={'light'} text={'dark'} key={i}>
-                        <Card.Body>
-                            <Card.Title>{data.name}</Card.Title>
-                            <Card.Text>
-                                <li>Population: {data.population}</li>
-                                <li>Latitude: {data.latitude}</li>
-                                <li>Longitude: {data.longitude}</li>
-                                <li><a href="Attractions">Attractions</a></li>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                ))
-            }
-        </div>        
+    <div className="backgroundCities">
+      <br />
+      <div
+        style={{border: "10px solid white",padding: "1px", backgroundColor: "white", display: "flex", alignItems: "center",justifyContent: "center", height: "100%"}}>
+        <p style={{fontSize: 50, textAlign: "center", fontWeight: "800", margin: "auto"}} >Discover Top-Rated Cities!</p>
+      </div>
+      <br/>
+      <Container>
+        <Row>
+          {data.map((city, i) => (
+            <Col key={i} xs={12} sm={6} md={4}>
+              <Card
+                style={{ width: "100%", margin: "20px" }}
+                border={"success"}
+                bg={"light"}
+                text={"dark"}
+              >
+                <Card.Body>
+                  <Card.Title>{city.name}</Card.Title>
+                  <Card.Text>
+                    <ul>
+                      <li>Population: {city.population}</li>
+                      <li>Latitude: {city.latitude}</li>
+                      <li>Longitude: {city.longitude}</li>
+                      <li>
+                        <a href="Attractions">Attractions</a>
+                      </li>
+                    </ul>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -1,40 +1,46 @@
-import Col from "react-bootstrap/Col";
-import NavBar from "./NavBar";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Card from 'react-bootstrap/Card';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import Button from 'react-bootstrap/Button';
-import natureImage from './destinationImage.jpeg';
-import '../App.css';
+import React from "react";
+import { Col, Container, Row, Card } from "react-bootstrap";
 import data from "./Data/Boston/6aQ3FGZYJ506O1qHQUvC5Q_boston-tea-party-ships-and-museum-boston_reviews.json";
-
 
 function Reviews() {
   return (
     <div className="backgroundReviews">
-        <br></br>
-        <div style={{ border: '10px solid white', padding: '1px', backgroundColor: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-        <p style={{fontSize: 50, textAlign: "center", fontWeight: "800", margin: "auto"}}>Explore Our Highly-Rated Travel Destinations</p>
-        </div>
-        <br></br>
-        <div>
-            {
-                data.map((data, i) => (
-                    <Card style={{width: "80%", margin: "100px"}} border={'success '} bg={'light'} text={'dark'} key={i}>
-                        <Card.Body>
-                            <Card.Title>{data.user.name}</Card.Title>
-                            <Card.Img src={data.user.image_url} style={{width: '200px'}}/>
-                            <Card.Text>
-                                <li><a href={data.url}>Link to Review!</a></li>
-                                <li>{data.text}</li>
-                                <li>Rating: {data.rating}</li>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                ))
-            }
-        </div>    
+      <br />
+      <div style={{border: "10px solid white", padding: "1px", backgroundColor: "white", display: "flex", alignItems: "center", justifyContent: "center",height: "100%"}}>
+        <p style={{fontSize: 50, textAlign: "center", fontWeight: "800", margin: "auto"}} > Discover Authentic Travel Experiences: User Reviews and Ratings </p>
+      </div>
+      <br />
+      <Container>
+        <Row>
+          {data.map((review, i) => (
+            <Col key={i} xs={12} sm={6} md={4}>
+              <Card
+                style={{ width: "100%", margin: "20px" }}
+                border={"success"}
+                bg={"light"}
+                text={"dark"}
+              >
+                <Card.Body>
+                  <Card.Title>{review.user.name}</Card.Title>
+                  <Card.Img
+                    src={review.user.image_url}
+                    style={{ width: "200px" }}
+                  />
+                  <Card.Text>
+                    <ul>
+                      <li>
+                        <a href={review.url}>Link to Review!</a>
+                      </li>
+                      <li>{review.text}</li>
+                      <li>Rating: {review.rating}</li>
+                    </ul>
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </div>
   );
 }
