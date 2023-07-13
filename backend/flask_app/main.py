b8be7048b4cf5cb0f2d77fcc9af13015e81f515e
Adarsh Ashok <adarsh_ashok@utexas.edu>
2023-07-13 15:36:24 -0500

    mysql_python

--- backend/flask_app/main.py
+++ backend/flask_app/main.py
@@ -2,19 +2,32 @@
 from flask import Flask, jsonify, request
 from db import get_cities, get_attractions, get_reviews
 
+
 app = Flask(__name__)
 
 @app.route('/cities', methods=['GET'])
 def cities():
-    return get_songs()  
+    city_data = get_cities()  
+    response = city_data
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('Access-Control-Allow-Methods', 'GET')
+    return response
 
 @app.route('/attractions', methods=['GET'])
 def attractions():
-    return get_attractions()
+    attractions_data = get_attractions()  
+    response = attractions_data
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('Access-Control-Allow-Methods', 'GET')
+    return response
     
 @app.route('/reviews', methods=['GET'])
-def songs():
-    return get_reviews()
+def reviews():
+    reviews_data = get_reviews()  
+    response = reviews_data
+    response.headers.set('Access-Control-Allow-Origin', '*')
+    response.headers.set('Access-Control-Allow-Methods', 'GET')
+    return response
 
 if __name__ == '__main__':
     app.run()
--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ import Cities from './Components/Cities.js';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import About from './Components/About.js';
+import { useState } from 'react';
 
 function App() {
+
+  const [city, setCity] = useState("Chicago");
+  const [attractions, setAttraction] = useState("9nUrDQ_REhU6sgcgXFAyfA");
+
   return (
       <div class="background">
         <BrowserRouter>
@@ -15,11 +20,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Menu />} />
             <Route path="/home" element={<Menu />} />
-            <Route path="/Reviews" element={<Reviews />} />
-            <Route path="/Attractions" element={<Attractions />} />
+            <Route path="/Reviews" element={<Reviews attractions={attractions} />} />
+            <Route path="/Attractions" element={<Attractions city={city} />} />
             <Route path="/About" element={<About />} />
-            <Route path="/Cities" element={<Cities />} />
-            <Route path="/Attractions" element={<Attractions />} />
+            <Route path="/Cities" element={<Cities city={city} setCity={setCity} />} />
           </Routes>
         </BrowserRouter>
       </div>
